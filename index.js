c04a28c04f3cbbeb24b1b667b41ec5623d3886a7
Shumilova Olha <shumilova.olha@gmail.com>
2022-10-15 09:24:21 +0200

    password encryption using bcrypt

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { registerValidator, loginValidator } from './validation.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import checkAuth from './utils/checkAuth.js';
+import { register, login, getMe } from './controllers/UserController.js';
+import handleValidationErrors from './utils/handleValidationErrors.js';
 mongoose
   .connect(
     'mongodb+srv://incertus-helga:wwwwww@cluster0.b8g1qcv.mongodb.net/?retryWrites=true&w=majority',
@@ -10,6 +12,13 @@ mongoose
   .then(() => console.log('server OK'))
   .catch((err) => console.log('error', err));
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post('/auth/login', loginValidator, handleValidationErrors, login);
+app.post('/auth/register', registerValidator, handleValidationErrors, register);
+app.get('/auth/me', checkAuth, getMe);
+
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
@@ -19,6 +28,3 @@ app.listen(5555, (err) => {
   }
   console.log('server ok');
 });
-
-app.use(express.json());
-app.use(cors());
